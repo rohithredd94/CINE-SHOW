08ba4dd3c585faec3a2b4305b504f2441de30d4b
Rohith Reddy <rohithreddyk94@gmail.com>
2017-07-29 15:41:51 -0500

    Live Search fixed

--- app_client/common/directives/search/search.controller.js
+++ app_client/common/directives/search/search.controller.js
@@ -4,8 +4,8 @@
     .module('meanApp')
     .controller('searchCtrl', searchCtrl);
 
-  searchCtrl.$inject = ['$location', 'meanData', 'movieData','authentication','searchservice','$rootScope','$window','$timeout'];
-  function searchCtrl($location, meanData, movieData, authentication,searchservice,$rootScope,$window,$timeout) {
+  searchCtrl.$inject = ['$location', 'meanData', 'movieData','authentication','searchservice','$rootScope','$window','$timeout','$scope'];
+  function searchCtrl($location, meanData, movieData, authentication,searchservice,$rootScope,$window,$timeout,$scope) {
     var vm = this;
     console.log('Search controller activated');
     vm.isLoggedIn = authentication.isLoggedIn();
@@ -17,6 +17,7 @@
       query : ""
     };
     vm.searchData = {};
+    vm.show = true;
 
     movieData.getAll()
       .success(function(data) {
@@ -28,13 +29,16 @@
       });
     console.log(vm.search);
     vm.onSearch = function(){
+      vm.show = false;
       movieData.getSearch(vm.search)
               .success(function(data){
                 console.log('success',data.length);
                 vm.searchData = data;
                 searchservice.addData(vm.searchData);
                 $timeout(function(){
-                  $rootScope.$broadcast('search_event');  
+                  $rootScope.$broadcast('search_event');
+                  $scope.$apply();
+                  console.log('Event broadcast complete');  
                 });
                 $location.path('/search');
                 //$window.location.assign("/search");
@@ -45,6 +49,38 @@
 
 
 
+  }
+
+  angular
+    .module('meanApp')
+    .directive('onFocus', onFocus);
+
+  function onFocus () {
+    return {
+      restrict: 'EA',
+      link: function(scope, elm, attrs) {
+            console.log('onfocus');
+            elm.bind('focus', function() {
+                scope.$apply(attrs.onFocus);
+            });
+      }
+    };
+  }
+
+  angular
+    .module('meanApp')
+    .directive('onBlur', onBlur);
+
+  function onBlur () {
+    return {
+      restrict: 'EA',
+      link: function(scope, elm, attrs) {
+            console.log('onblur');
+            elm.bind('blur', function() {
+                scope.$apply(attrs.onBlur);
+            });
+      }
+    };
   }
 
 })();
